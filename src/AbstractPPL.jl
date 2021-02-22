70f385183dd50ee03ea9bd0defebb72388d01898
Philipp Gabler <phipsgabler@users.noreply.github.com>
2021-02-22 17:13:16 +0100

    First draft of model abstraction

--- src/AbstractPPL.jl
+++ src/AbstractPPL.jl
@@ -2,23 +2,24 @@ module AbstractPPL
 
 
 include("varname.jl")
-include("abstractmodel.jl")
-include("abstractvarinfo.jl")
+include("contexts.jl")
+include("abstractpp.jl")
 
 
 # Abstract model functions
-export AbstractModel
-export logjoint,
-    logprior
+export AbstractProbabilisticProgram
+export generate,
+    logdensity,
+    logjoint,
+    loglikelihood,
+    logprior,
+    sample
 
 # VarName
 export VarName,
     inspace,
     subsumes
 export @varname
-
-# VarInfo
-export AbstractVarInfo
 
 
 end # module
