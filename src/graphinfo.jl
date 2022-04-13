ed16f90d1469bcaa07c70d3119d26ab523a1362d
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2022-04-13 12:02:52 +0000

    Fixing type stability issues for constructors (#65)
    Fixes #64 by adding more type parameters to the constructors to make the `NamedTuples` concrete.

--- src/graphinfo.jl
+++ src/graphinfo.jl
@@ -20,11 +20,11 @@ adjacency matrix and topologically ordered vertex list and stored.
 GraphInfo is instantiated using the `Model` constctor. 
 """
 
-struct GraphInfo{T} <: AbstractModelTrace
-    input::NamedTuple{T}
-    value::NamedTuple{T}
-    eval::NamedTuple{T}
-    kind::NamedTuple{T}
+struct GraphInfo{Tnames, Tinput, Tvalue, Teval, Tkind} <: AbstractModelTrace
+    input::NamedTuple{Tnames, Tinput}
+    value::NamedTuple{Tnames, Tvalue}
+    eval::NamedTuple{Tnames, Teval}
+    kind::NamedTuple{Tnames, Tkind}
     A::SparseMatrixCSC
     sorted_vertices::Vector{Symbol}
 end
@@ -55,8 +55,8 @@ y = (value = 0.0, input = (:μ, :s2), eval = var"#7#10"(), kind = :Stochastic)
 ```
 """
 
-struct Model{T} <: AbstractProbabilisticProgram
-    g::GraphInfo{T}
+struct Model{Tnames, Tinput, Tvalue, Teval, Tkind} <: AbstractProbabilisticProgram
+    g::GraphInfo{Tnames, Tinput, Tvalue, Teval, Tkind}
 end
 
 function Model(;kwargs...)
