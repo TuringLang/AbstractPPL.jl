4a222d2241cc76c8bbb9c18a7c8f11f45d28754c
Xianda Sun <sunxdt@gmail.com>
2024-10-27 17:28:45 +0000

    add sentence following Penny's comment

--- src/abstractprobprog.jl
+++ src/abstractprobprog.jl
@@ -67,7 +67,8 @@ function condition end
 
 Fix the values of parameters specified in `params` within the probabilistic model `model`. 
 This operation is equivalent to treating the fixed parameters as being drawn from a point mass 
-distribution centered at the values specified in `params`.
+distribution centered at the values specified in `params`. Thus these parameters no longer contribute
+to the accumulated log density. 
 
 Conceptually, this is similar to Pearl's do-operator in causal inference, where we intervene 
 on variables by setting them to specific values, effectively cutting off their dependencies 
