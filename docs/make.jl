3ae96eea8c0d67a0334f88ce75483b9b73a737eb
Penelope Yong <penelopeysm@gmail.com>
2024-12-05 14:37:18 +0000

    Format

--- docs/make.jl
+++ docs/make.jl
@@ -2,15 +2,12 @@ using Documenter
 using AbstractPPL
 
 # Doctest setup
-DocMeta.setdocmeta!(AbstractPPL, :DocTestSetup, :(using AbstractPPL); recursive=true)
+DocMeta.setdocmeta!(AbstractPPL, :DocTestSetup, :(using AbstractPPL); recursive = true)
 
 makedocs(;
-    sitename="AbstractPPL",
-    modules=[AbstractPPL],
-    pages=[
-        "Home" => "index.md",
-        "API" => "api.md",
-    ],
-    checkdocs=:exports,
-    doctest=false,
+    sitename = "AbstractPPL",
+    modules = [AbstractPPL],
+    pages = ["Home" => "index.md", "API" => "api.md"],
+    checkdocs = :exports,
+    doctest = false,
 )
