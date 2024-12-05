1c5408b27e2407bb3e95379cc8ed658a5fc9d306
Penelope Yong <penelopeysm@gmail.com>
2024-12-05 14:52:18 +0000

    Use new GitHub actions (#110)
    * Use new GitHub actions
    
    * Tidy names
    
    * Add Format action
    
    * Format
    
    * Add JuliaFormatter config

--- docs/make.jl
+++ docs/make.jl
@@ -2,17 +2,12 @@ using Documenter
 using AbstractPPL
 
 # Doctest setup
-DocMeta.setdocmeta!(AbstractPPL, :DocTestSetup, :(using AbstractPPL); recursive=true)
+DocMeta.setdocmeta!(AbstractPPL, :DocTestSetup, :(using AbstractPPL); recursive = true)
 
 makedocs(;
-    sitename="AbstractPPL",
-    modules=[AbstractPPL],
-    pages=[
-        "Home" => "index.md",
-        "API" => "api.md",
-    ],
-    checkdocs=:exports,
-    doctest=false,
+    sitename = "AbstractPPL",
+    modules = [AbstractPPL],
+    pages = ["Home" => "index.md", "API" => "api.md"],
+    checkdocs = :exports,
+    doctest = false,
 )
-
-deploydocs(; repo="github.com/TuringLang/AbstractPPL.jl.git", push_preview=true)
