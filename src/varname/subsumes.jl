0bbaf31d9d4fcc186790ecdf2c9d9a0a403a97da
Penelope Yong <penelopeysm@gmail.com>
2025-12-23 23:43:29 +0000

    subsumes works

--- src/varname/subsumes.jl
+++ src/varname/subsumes.jl
@@ -12,15 +12,28 @@ julia> subsumes(@varname(x[1, 2]), @varname(x[1, 2][3]))
 true
 ```
 
-Note that often this is not possible to determine statically. For example:
+This is done by recursively comparing each layer of the VarNames' optics.
 
-- When dynamic indices are present, subsumption cannot be determined, unless `child ==
-  parent`.
+Note that often this is not possible to determine statically, and so the results should
+not be over-interpreted. In particular, `Index` optics  pose a problem. An `i::Index` will
+only subsume `j::Index` if:
+
+1. They have the same number of positional indices (`i.ix` and `j.ix`);
+2. Each positional index in `i` can be determined to comprise the corresponding positional
+   index in `j`; and
+3. The keyword indices of `i` (`i.kw`) are a superset of those in `j.kw`).
+
+In all other cases, `subsumes` will conservatively return `false`, even though in practice
+it might well be that `i` does subsume `j`. Some examples where subsumption cannot be
+determined statically are:
+
 - Subsumption between different forms of indexing is not supported, e.g. `x[4]` and `x[2,
   2]` are not considered to subsume each other, even though they might in practice (e.g. if
   `x` is a 2x2 matrix).
-
-In such cases, `subsumes` will conservatively return `false`.
+- When dynamic indices (that are not equal) are present. (Dynamic indices that are equal do
+  subsume each other.)
+- Non-standard indices, e.g. `Not(4)`, `2..3`, etc. Again, these only subsume each other
+  when they are equal.
 """
 function subsumes(u::VarName, v::VarName)
     return getsym(u) == getsym(v) && subsumes(getoptic(u), getoptic(v))
@@ -34,121 +47,30 @@ subsumes(::Property, ::Index) = false
 subsumes(::Index, ::Property) = false
 
 function subsumes(i::Index, j::Index)
-    # TODO(penelopeysm): What we really want to do is to zip i.ix and j.ix
-    # and check that each index in `i.ix` subsumes the corresponding
-    # entry in `j.ix`. If that is true, then we can continue recursing.
-    return if i.ix == j.ix && i.kw == j.kw
-        subsumes(i.child, j.child)
-    else
-        error("Not implemented.")
+    return _subsumes_positional(i.ix, j.ix) &&
+           _subsumes_keyword(i.kw, j.kw) &&
+           subsumes(i.child, j.child)
+end
+
+function _subsumes_positional(i::Tuple, j::Tuple)
+    return (length(i) == length(j)) && all(_subsumes_index.(i, j))
+end
+function _subsumes_keyword(i::NamedTuple{f1}, j::NamedTuple{f2}) where {f1,f2}
+    for name in f2
+        if !(name in f1) || !(_subsumes_index(i[name], j[name]))
+            return false
+        end
     end
+    return true
 end
 
-#=
-# Since expressions such as `x[:][:][:][1]` and `x[1]` are equal,
-# the indexing behavior must be considered jointly.
-# Therefore we must recurse until we reach something that is NOT
-# indexing, and then consider the sequence of indices leading up to this.
-"""
-    subsumes_indices(t, u)
-
-Return `true` if the indexing represented by `t` subsumes `u`.
-
-This is mostly useful for comparing compositions involving `IndexLens`
-e.g. `_[1][2].a[2]` and `_[1][2].a`. In such a scenario we do the following:
-1. Combine `[1][2]` into a `Tuple` of indices using [`combine_indices`](@ref).
-2. Do the same for `[1][2]`.
-3. Compare the two tuples from (1) and (2) using `subsumes_indices`.
-4. Since we're still undecided, we call `subsume(@o(_.a[2]), @o(_.a))`
-   which then returns `false`.
-
-# Example
-```jldoctest; setup=:(using Accessors; using AbstractPPL: subsumes_indices)
-julia> t = @o(_[1].a); u = @o(_[1]);
-
-julia> subsumes_indices(t, u)
-false
-
-julia> subsumes_indices(u, t)
-true
-
-julia> # `identity` subsumes all.
-       subsumes_indices(identity, t)
-true
-
-julia> # None subsumes `identity`.
-       subsumes_indices(t, identity)
-false
-
-julia> AbstractPPL.subsumes(@o(_[1][2].a[2]), @o(_[1][2].a))
-false
-
-julia> AbstractPPL.subsumes(@o(_[1][2].a), @o(_[1][2].a[2]))
-true
-```
-"""
-function subsumes_indices(t::ALLOWED_OPTICS, u::ALLOWED_OPTICS)
-    t_indices, t_next = combine_indices(t)
-    u_indices, u_next = combine_indices(u)
-
-    # If we already know that `u` is not subsumed by `t`, return early.
-    if !subsumes_indices(t_indices, u_indices)
-        return false
-    end
-
-    if t_next === nothing
-        # Means that there's nothing left for `t` and either nothing
-        # or something left for `u`, i.e. `t` indeed `subsumes` `u`.
-        return true
-    elseif u_next === nothing
-        # If `t_next` is not `nothing` but `u_next` is, then
-        # `t` does not subsume `u`.
-        return false
-    end
-
-    # If neither is `nothing` we continue.
-    return subsumes(t_next, u_next)
-end
-
-"""
-    combine_indices(optic)
-
-Return sequential indexing into a single `Tuple` of indices,
-e.g. `x[:][1][2]` becomes `((Colon(), ), (1, ), (2, ))`.
-
-The result is compatible with [`subsumes_indices`](@ref) for `Tuple` input.
-"""
-combine_indices(optic::ALLOWED_OPTICS) = (), optic
-combine_indices(optic::IndexLens) = (optic.indices,), nothing
-function combine_indices(optic::ComposedFunction{<:ALLOWED_OPTICS,<:IndexLens})
-    indices, next = combine_indices(optic.outer)
-    return (optic.inner.indices, indices...), next
-end
-
-"""
-    subsumes_indices(left_indices::Tuple, right_indices::Tuple)
-
-Return `true` if `right_indices` is subsumed by `left_indices`.  `left_indices` is assumed to be 
-concretized and consist of either `Int`s or `AbstractArray`s of scalar indices that are supported 
-by array A.
-
-Currently _not_ supported are: 
-- Boolean indexing, literal `CartesianIndex` (these could be added, though)
-- Linear indexing of multidimensional arrays: `x[4]` does not subsume `x[2, 2]` for a matrix `x`
-- Trailing ones: `x[2, 1]` does not subsume `x[2]` for a vector `x`
-"""
-subsumes_indices(::Tuple{}, ::Tuple{}) = true  # x subsumes x
-subsumes_indices(::Tuple{}, ::Tuple) = true    # x subsumes x...
-subsumes_indices(::Tuple, ::Tuple{}) = false   # x... does not subsume x
-function subsumes_indices(t1::Tuple, t2::Tuple)  # does x[i]... subsume x[j]...?
-    first_subsumed = all(Base.splat(subsumes_index), zip(first(t1), first(t2)))
-    return first_subsumed && subsumes_indices(Base.tail(t1), Base.tail(t2))
-end
-
-subsumes_index(i::Colon, ::Colon) = error("Colons cannot be subsumed")
-subsumes_index(i, ::Colon) = error("Colons cannot be subsumed")
-# Necessary to avoid ambiguity errors.
-subsumes_index(::AbstractVector, ::Colon) = error("Colons cannot be subsumed")
-subsumes_index(i::Colon, j) = true
-subsumes_index(i::AbstractVector, j) = issubset(j, i)
-subsumes_index(i, j) = i == j =#
+_subsumes_index(a::DynamicIndex, b::DynamicIndex) = a == b
+_subsumes_index(a::DynamicIndex, ::Any) = false
+_subsumes_index(::DynamicIndex, ::Colon) = false
+_subsumes_index(::Colon, ::DynamicIndex) = true
+_subsumes_index(::Any, ::DynamicIndex) = false
+_subsumes_index(::Colon, ::Any) = true
+_subsumes_index(::Any, ::Colon) = false
+_subsumes_index(a::AbstractVector, b::Any) = issubset(b, a)
+_subsumes_index(a::AbstractVector, b::Colon) = false
+_subsumes_index(a::Any, b::Any) = a == b
--- test/varname/subsumes.jl
+++ test/varname/subsumes.jl
@@ -4,51 +4,72 @@ using AbstractPPL
 using Test
 
 @testset "varname/subsumes.jl" verbose = true begin
-    @test subsumes(@varname(x), @varname(x))
-    @test subsumes(@varname(x[1]), @varname(x[1]))
-    @test subsumes(@varname(x.a), @varname(x.a))
+    @testset "varnames that are equal" begin
+        @test subsumes(@varname(x), @varname(x))
+        @test subsumes(@varname(x[1]), @varname(x[1]))
+        @test subsumes(@varname(x.a), @varname(x.a))
+    end
 
     uncomparable(vn1, vn2) = !subsumes(vn1, vn2) && !subsumes(vn2, vn1)
-    @test uncomparable(@varname(x), @varname(y))
-    @test uncomparable(@varname(x.a), @varname(y.a))
-    @test uncomparable(@varname(a.x), @varname(a.y))
-    @test uncomparable(@varname(a.x[1]), @varname(a.x.z))
-    @test uncomparable(@varname(x[1]), @varname(y[1]))
-    @test uncomparable(@varname(x[1]), @varname(x.y))
+    @testset "uncomparable varnames" begin
+        @test uncomparable(@varname(x), @varname(y))
+        @test uncomparable(@varname(x.a), @varname(y.a))
+        @test uncomparable(@varname(a.x), @varname(a.y))
+        @test uncomparable(@varname(a.x[1]), @varname(a.x.z))
+        @test uncomparable(@varname(x[1]), @varname(y[1]))
+        @test uncomparable(@varname(x[1]), @varname(x.y))
+    end
 
     strictly_subsumes(vn1, vn2) = subsumes(vn1, vn2) && !subsumes(vn2, vn1)
-    # Subsumption via field/indexing
-    @test strictly_subsumes(@varname(x), @varname(x.a))
-    @test strictly_subsumes(@varname(x), @varname(x[1]))
-    @test strictly_subsumes(@varname(x), @varname(x[2:2:5]))
-    @test strictly_subsumes(@varname(x), @varname(x[10, 20]))
-    @test strictly_subsumes(@varname(x.a), @varname(x.a.b))
-    @test strictly_subsumes(@varname(x[1]), @varname(x[1].a))
-    @test strictly_subsumes(@varname(x.a), @varname(x.a[1]))
-    @test strictly_subsumes(@varname(x[1:10]), @varname(x[1:10][2]))
-    # Range subsumption
-    @test strictly_subsumes(@varname(x[1:10]), @varname(x[1]))
-    @test strictly_subsumes(@varname(x[1:10]), @varname(x[1:5]))
-    @test strictly_subsumes(@varname(x[1:10]), @varname(x[4:6]))
-    @test strictly_subsumes(@varname(x[1:10, 1:10]), @varname(x[1:5, 1:5]))
-    @test strictly_subsumes(@varname(x[[7, 6, 5, 4, 3, 2, 1]]), @varname(x[[2, 3, 5]]))
+    @testset "strict subsumption - no index comparisons" begin
+        @test strictly_subsumes(@varname(x), @varname(x.a))
+        @test strictly_subsumes(@varname(x), @varname(x[1]))
+        @test strictly_subsumes(@varname(x), @varname(x[2:2:5]))
+        @test strictly_subsumes(@varname(x), @varname(x[10, 20]))
+        @test strictly_subsumes(@varname(x.a), @varname(x.a.b))
+        @test strictly_subsumes(@varname(x[1]), @varname(x[1].a))
+        @test strictly_subsumes(@varname(x.a), @varname(x.a[1]))
+        @test strictly_subsumes(@varname(x[1:10]), @varname(x[1:10][2]))
+    end
 
-    # TODO reenable
-    # @test_strict_subsumption x[:a][1] x[:a]
-    # # boolean indexing works as long as it is concretized
-    # A = rand(10, 10)
-    # @test @varname(A[iseven.(1:10), 1], true) ⊑ @varname(A[1:10, 1])
-    # @test @varname(A[iseven.(1:10), 1], true) ⋣ @varname(A[1:10, 1])
-    #
-    # # we can reasonably allow colons on the right side ("universal set")
-    # @test @varname(x[1]) ⊑ @varname(x[:])
-    # @test @varname(x[1:10, 1]) ⊑ @varname(x[:, 1:10])
-    # @test_throws ErrorException (@varname(x[:]) ⊑ @varname(x[1]))
-    # @test_throws ErrorException (@varname(x[:]) ⊑ @varname(x[:]))
-    #
-    # TODO dynamic indices
-    #
-    # TODO keyword indices
+    @testset "strict subsumption - index comparisons" begin
+        @testset "integer vectors" begin
+            @test strictly_subsumes(@varname(x[1:10]), @varname(x[1]))
+            @test strictly_subsumes(@varname(x[1:10]), @varname(x[1:5]))
+            @test strictly_subsumes(@varname(x[1:10]), @varname(x[4:6]))
+            @test strictly_subsumes(@varname(x[1:10, 1:10]), @varname(x[1:5, 1:5]))
+            @test strictly_subsumes(@varname(x[[5, 4, 3, 2, 1]]), @varname(x[[2, 4]]))
+        end
+
+        @testset "non-integer indices" begin
+            @test strictly_subsumes(@varname(x[:a]), @varname(x[:a][1]))
+        end
+
+        @testset "colon" begin
+            @test strictly_subsumes(@varname(x[:]), @varname(x[1]))
+            @test strictly_subsumes(@varname(x[:, 1:10]), @varname(x[1:10, 1]))
+        end
+
+        @testset "dynamic indices" begin
+            @test strictly_subsumes(@varname(x), @varname(x[begin]))
+            @test subsumes(@varname(x[begin]), @varname(x[begin]))
+            @test strictly_subsumes(@varname(x[:]), @varname(x[begin]))
+            @test strictly_subsumes(@varname(x), @varname(x[end]))
+            @test subsumes(@varname(x[end]), @varname(x[end]))
+            @test strictly_subsumes(@varname(x[:]), @varname(x[end]))
+            @test strictly_subsumes(@varname(x[:]), @varname(x[1:end]))
+            @test strictly_subsumes(@varname(x[:]), @varname(x[end - 3]))
+        end
+
+        @testset "keyword indices" begin
+            @test strictly_subsumes(@varname(x), @varname(x[a=1]))
+            @test strictly_subsumes(@varname(x[a=1:10, b=1:10]), @varname(x[a=1:10]))
+            @test strictly_subsumes(@varname(x[a=1:10, b=1:10]), @varname(x[a=1:5, b=1:5]))
+            @test strictly_subsumes(@varname(x[a=:]), @varname(x[a=1]))
+            @test uncomparable(@varname(x[a=1:10, b=5]), @varname(x[a=5, b=1:10]))
+            @test uncomparable(@varname(x[a=1]), @varname(x[b=1]))
+        end
+    end
 end
 
 end # module
