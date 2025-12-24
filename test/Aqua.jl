0282e150074baea6fd86670c1ce779db0fda3df3
Penelope Yong <penelopeysm@gmail.com>
2025-12-24 18:28:40 +0000

    reenable Aqua tests

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -3,8 +3,6 @@ module AquaTests
 using Aqua: Aqua
 using AbstractPPL
 
-# For now, we skip ambiguities since they come from interactions
-# with third-party packages rather than issues in AbstractPPL itself
-Aqua.test_all(AbstractPPL; ambiguities=false)
+Aqua.test_all(AbstractPPL)
 
 end
